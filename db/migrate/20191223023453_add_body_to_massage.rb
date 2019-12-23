88c97a3408def67b24626df40e7d9f0f6029517f
hideo-0103 <59080025+hideo-0103@users.noreply.github.com>
2019-12-23 11:37:26 +0900

    Create 20191223023453_add_body_to_massage.rb

---
+++ db/migrate/20191223023453_add_body_to_massage.rb
@@ -0,0 +1,8 @@
+class AddBodyToMassage < ActiveRecord::Migration[5.0]
+  def change
+    add_column :massages, :body, :text
+    add_column :massages, :image, :string
+    add_column :massages, :user_id, :integer
+    add_column :massages, :group_id, :integer
+  end
+end

29b9cadb6c6d580c2c7cdfc52d2d1b107cfc0aff
hideo-0103 <59080025+hideo-0103@users.noreply.github.com>
2019-12-23 11:05:52 +0900

    create group model file, add column

---
+++ db/migrate/20191223020259_add_name_to_group.rb
@@ -0,0 +1,8 @@
+class AddNameToGroup < ActiveRecord::Migration[5.0]
+  def change
+    add_column :groups, :name, :string
+    add_column :groups, :boby, :text
+    add_column :groups, :image, :string
+    add_column :groups, :user_id, :integer
+  end
+end

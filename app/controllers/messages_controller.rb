2de84b78a6f78f5ff83be838740dbf458a1ddfe9
hideo-0103 <59080025+hideo-0103@users.noreply.github.com>
2019-12-26 20:54:03 +0900

    createアクション定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,14 @@ class MessagesController < ApplicationController
     user = current_user
     @nickname = user.name
   end
+
+  def create
+
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:image, :text).merge(user_id: current_user.id)
+  end
 end

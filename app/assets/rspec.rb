3934b0d987411c2958df22955ade0ac14d5f86c8
hideo-0103 <59080025+hideo-0103@users.noreply.github.com>
2020-02-07 18:40:03 +0900

    テス

---
+++ app/assets/rspec.rb
@@ -0,0 +1,34 @@
+def rspecskipper(instance,columns)
+  model = instance.capitalize
+  i = 0
+  while i < columns.length do
+    ellorColumn = columns[i]
+
+    puts 'describe #{model} do\ndescribe '#create' do\nit \'is invalid without a #{ellorColumn} \' do\n#{instance} = build(:#{instance}, #{ellorColumn}: nil)\n#{instance}.valid?\nexpect(#{instance}.errors[:#{ellorColumn}]).to include()\nend\nend\nend'
+    i += 1
+  end
+end
+
+#↓の配列の中にカラム名を入れる
+productColumns = ['price','size','statusstring','delivery_datestring','name','descriptiontext','first_category_id','bland_id','buyer_id','saler_id']
+
+def openArray(array)
+  newArray = [] 
+  array.each do |a|
+    newA = '#{a}              {\'abe\'}\n'
+    newArray << newA
+  end
+  t = newArray.join('')
+end
+classMessage = openArray(productColumns)
+puts 'FactoryBot.define do
+
+  factory :user do\n
+    #{classMessage}
+
+  end
+
+end'
+
+#↓テスト文を生成する第一引数はモデル名(先頭の文字は小文字にすること)、第二引数はカラムの配列
+rspecskipper('product',productColumns)
